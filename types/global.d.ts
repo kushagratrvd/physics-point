77ab48fa966766bfd9e3fa05ebcaf0c3873a73de
Kushagra <kushagratrivedi962@gmail.com>
2025-04-04 21:24:46 +0530

    components and global.d.ts

---
+++ types/global.d.ts
@@ -0,0 +1,20 @@
+interface Window {
+  __ow?: {
+    organizationId: string
+    integration_name: string
+    product_name: string
+    asyncInit?: boolean
+  }
+  OpenWidget?: {
+    _q: any[]
+    _h: any
+    _v: string
+    on: Function
+    once: Function
+    off: Function
+    get: Function
+    call: Function
+    init: Function
+  }
+}
+
